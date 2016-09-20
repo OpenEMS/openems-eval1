9fd278f135e745660298dbf54d7148edaf76033b
Stefan Feilmeier <stefan.feilmeier@fenecon.de>
2016-09-20 21:47:46 +0200

    Simple working OpenEMS Application bundle

---
+++ io.openems.application/src/io/openems/application/OpenemsApplication.java
@@ -0,0 +1,15 @@
+package io.openems.application;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+
+@Component(name="io.openems")
+public class OpenemsApplication {
+
+	@Activate
+	public void activate() {
+		System.out.println();
+		System.out.println("Activate OpenemsApplication");
+	}
+	
+}
